c6e1087f9fb46269d7891e6dadb5013ba2b34137
G-Salis <giuseppesalislavoro@gmail.com>
2021-10-26 15:51:39 +0200

    Bonus estetico

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
+const myCol = document.getElementById('mionumero');
+const pcCol = document.getElementById('pcnumero');
+const risCol = document.getElementById('ris');
+
 
 //MAIL
 let dbEmail = ['pippolino@mail.com', 'linopippo@mail.com'];
@@ -20,34 +24,43 @@ for(let i = 0; i < dbEmail.length; i++){
 
 }
 
+const box = document.createElement("div");
+  box.classList.add("box");
+  myCol.append(box);
+  pcCol.append(box);
+  risCol.append(box);
+
 if (registrazione == true) {
   alert("La tua mail è valida, puoi giocare");
 
   //DADI
 const playerDice = Math.floor(Math.random() * 5) + 1;
+
+myCol.append("il tuo numero è: ");
+myCol.append(playerDice);
+myCol.classList.add("box");
+
+
 const pcDice = Math.floor(Math.random() * 5) + 1;
+
+pcCol.append("il numero del pc è: ");
+pcCol.append(pcDice);
+pcCol.classList.add("box");
 
 if(playerDice > pcDice){
 
-  let risultato = `
-  Tu hai scelto il numero ${playerDice}<br>
-  il PC ha scelto il numero ${pcDice}.<br>
-  <strong>HAI VINTO!</strong>
+  box.append("HAI VINTO!");
+  box.classList.add("boxwin");
 
-`
-document.getElementById('output').innerHTML = risultato;
+}else if(playerDice < pcDice){
 
+  box.append("HAI PERSO!");
+  box.classList.add("boxlose");
 
 }else{
 
-  let risultato = `
-  Tu hai scelto il numero ${playerDice}<br>
-  il PC ha scelto il numero ${pcDice}.<br>
-  <strong>HAI PERSO!</strong>
-
-`
-document.getElementById('output').innerHTML = risultato;
-
+  box.append("PAREGGIO!");
+  box.classList.add("box");
 
 }
 
